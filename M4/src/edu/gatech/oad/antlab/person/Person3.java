4eb5cf3e2ee5a8e6d9886ddb90e043cb95bafa49
tstowel1 <tstowell3@gatech.edu>
2013-02-15 12:30:07 -0500

    Update Person3.java

--- M4/src/edu/gatech/oad/antlab/person/Person3.java
+++ M4/src/edu/gatech/oad/antlab/person/Person3.java
@@ -42,7 +42,10 @@ public class Person3 {
 	 * @return the reversed string
 	 */
 	private String calc(String input) {
-	  //Person 3 put your implementation here
-	  return null;
+	  String s = "";
+	  for (int i=0; i<input.length(); i++) {
+	  	s = input.charAt(i) + s;
+	  }
+	  return s;
 	}
 }
