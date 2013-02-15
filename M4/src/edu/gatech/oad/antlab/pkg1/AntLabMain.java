58623223bbe37488551aa351262f3b5bddfe09a2
Zachary Waldowski <zwaldowski@gmail.com>
2013-02-15 11:51:53 -0500

    Unfortunately, we have no Person 5. :-( Remove the reference to them.

--- M4/src/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M4/src/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,10 +73,6 @@ import edu.gatech.oad.antlab.pkg2.*;
           //and ggg000x with your gt num
           Person4 p4 = new Person4("P4");
           toPrint += p4.toString("ggg004x");
-		  //Person5 replace P5 with your name
-          //and ggg000x with your gt num
-          Person5 p5 = new Person5("P5");
-          toPrint += p5.toString("ggg005x");
         System.out.println(toPrint);
         
         
