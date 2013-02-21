71d98e49b8c2939e5deb9bef0045af9c4c9ac752
jcole123 <jcole12321@gmail.com>
2013-02-21 17:32:10 -0500

    Created login and member class as well as a text file to store user info until we actually work on a database

---
+++ FindMyThings/src/edu/gatech/cs2340_sp13/teamrocket/findmythings/Login.java
@@ -0,0 +1,34 @@
+package edu.gatech.cs2340_sp13.teamrocket.findmythings;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+
+
+public class Login {
+	
+	public Login() {
+		
+	}
+	public boolean verifyUser(Member m) {
+		Scanner scan = new Scanner(nopasswordsinhere.txt);
+		boolean found = false;
+				while(scan.hasNext() && !found) {
+					String[] str = scan.nextLine().split(":");
+					if(m.getUser.equals(str))	
+						found=true;
+				}
+	}
+	public void register(String name, String password) {
+		try {
+		    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("nopasswordsinhere.txt", true)));  //store usernames and passwords in super secure text file
+		    out.println(name + ":" + password);
+		    out.close();
+		} catch (IOException e) {
+		    System.out.println("This can't be life");
+		}
+	}
+
+}
---
+++ FindMyThings/src/edu/gatech/cs2340_sp13/teamrocket/findmythings/Member.java
@@ -0,0 +1,7 @@
+package edu.gatech.cs2340_sp13.teamrocket.findmythings;
+
+public class Member {
+	private String user, password, name, phone;
+	//private Location address; 
+	public Member()
+}
