2badb325a5a83e2a631b9c41f63c9d75a2abb915
jcole123 <jcole12321@gmail.com>
2013-03-14 16:49:32 -0400

    Using the magic of Android the search bar now works.

--- FindMyThings/src/edu/gatech/cs2340_sp13/teamrocket/findmythings/AdminActivity.java
+++ FindMyThings/src/edu/gatech/cs2340_sp13/teamrocket/findmythings/AdminActivity.java
@@ -3,8 +3,11 @@ package edu.gatech.cs2340_sp13.teamrocket.findmythings;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 public class AdminActivity extends ListActivity {
@@ -14,20 +17,55 @@ public class AdminActivity extends ListActivity {
 	 */
 	private ArrayAdapter<Member> mUsers;
 	
+	/**
+	 * Reference to the list that the ArrayAdaper will generate
+	 */
+	private ListView mList;
+	
+	/**
+	 * Reference to search bar
+	 */
+	private EditText mSearch;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+			
 		setContentView(R.layout.activity_admin_layout);
+		
+		mSearch = (EditText)findViewById(R.id.search_bar);
+		mList = (ListView)findViewById(android.R.id.list);
+		
 		//Instantiate ArrayAdapter and tell it to display the list of Members from Login
 		mUsers = new ArrayAdapter<Member>(this,
 				android.R.layout.simple_list_item_activated_1,
 				android.R.id.text1, Login.getData());
-		setListAdapter(mUsers);
+		
+		
+		mList.setAdapter(mUsers);
+		
+		//Allow user to filter by text
+		mList.setTextFilterEnabled(true);
+		
+		mSearch.addTextChangedListener(new TextWatcher()
+	    {
+	        @Override
+	        public void onTextChanged( CharSequence s, int arg1, int arg2, int arg3) {	        
+	        	mUsers.getFilter().filter(s);
+	        	mUsers.notifyDataSetChanged();
+	        }
+	        @Override
+	        public void beforeTextChanged( CharSequence arg0, int arg1, int arg2, int arg3) {}
+	        
+	        @Override
+	        public void afterTextChanged(Editable arg0) {}
+			
+	    });
 	}
 	
 	@Override
 	protected void onListItemClick (ListView l, View v, int position, long id) {
+		//TODO: make some kind of activity or view to allow admin to change user attributes
 		super.onListItemClick(l, v, position, id);
 		mUsers.remove(Login.getData().get((int) id));
 	}
