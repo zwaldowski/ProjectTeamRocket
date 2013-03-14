4b253c041535c99e23fe00a5a3e2345abb8ecf38
jcole123 <jcole12321@gmail.com>
2013-03-14 16:31:51 -0400

    Added search bar with no functionality and item listener to AdminActivity

--- FindMyThings/src/edu/gatech/cs2340_sp13/teamrocket/findmythings/AdminActivity.java
+++ FindMyThings/src/edu/gatech/cs2340_sp13/teamrocket/findmythings/AdminActivity.java
@@ -3,7 +3,9 @@ package edu.gatech.cs2340_sp13.teamrocket.findmythings;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class AdminActivity extends ListActivity {
 
@@ -15,11 +17,19 @@ public class AdminActivity extends ListActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//make our presenter
+		
+		setContentView(R.layout.activity_admin_layout);
+		//Instantiate ArrayAdapter and tell it to display the list of Members from Login
 		mUsers = new ArrayAdapter<Member>(this,
 				android.R.layout.simple_list_item_activated_1,
 				android.R.id.text1, Login.getData());
 		setListAdapter(mUsers);
+	}
+	
+	@Override
+	protected void onListItemClick (ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		mUsers.remove(Login.getData().get((int) id));
 	}
 	
 
