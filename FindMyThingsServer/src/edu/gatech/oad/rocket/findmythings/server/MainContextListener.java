8e28e50912e296823c066d5f6607c63a54145229
Zachary Waldowski <zwaldowski@gmail.com>
2013-03-30 14:47:47 -0400

    Simplify the amount of injection necessary to maintain DatabaseService.

--- FindMyThingsServer/src/edu/gatech/oad/rocket/findmythings/server/MainContextListener.java
+++ FindMyThingsServer/src/edu/gatech/oad/rocket/findmythings/server/MainContextListener.java
@@ -24,7 +24,6 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Key;
 import com.google.inject.Provides;
-import com.google.inject.Scopes;
 import com.google.inject.Singleton;
 import com.google.inject.binder.AnnotatedBindingBuilder;
 import com.google.inject.name.Names;
@@ -69,7 +68,6 @@ public class MainContextListener extends GuiceServletContextListener {
 	    }
 
 		@Override protected void configureServlets() {
-			bind(ObjectifyFilter.class).in(Scopes.SINGLETON);
 			filter("/*").through(ObjectifyFilter.class);
 	        bind(PageGenerator.class).toInstance(createPageGenerator());
 	        bindString("email.from", Config.APP_EMAIL);
--- FindMyThingsServer/src/edu/gatech/oad/rocket/findmythings/server/db/DatabaseService.java
+++ FindMyThingsServer/src/edu/gatech/oad/rocket/findmythings/server/db/DatabaseService.java
@@ -6,9 +6,6 @@ import com.google.inject.Singleton;
 import com.googlecode.objectify.Objectify;
 import com.googlecode.objectify.ObjectifyFactory;
 import com.googlecode.objectify.ObjectifyService;
-import com.googlecode.objectify.cmd.Loader;
-import com.googlecode.objectify.util.cmd.LoaderWrapper;
-import com.googlecode.objectify.util.cmd.ObjectifyWrapper;
 
 import edu.gatech.oad.rocket.findmythings.server.model.AppAdmin;
 import edu.gatech.oad.rocket.findmythings.server.model.AppMember;
@@ -24,8 +21,8 @@ public abstract class DatabaseService {
 	/**
 	 * @return our extension to Objectify
 	 */
-	public static DatabaseManager ofy() {
-		return (DatabaseManager)ObjectifyService.ofy();
+	public static final Objectify ofy() {
+		return ObjectifyService.ofy();
 	}
 
 	/**
@@ -33,19 +30,6 @@ public abstract class DatabaseService {
 	 */
 	public static DatabaseFactory factory() {
 		return (DatabaseFactory)ObjectifyService.factory();
-	}
-
-	public static class DatabaseManager extends ObjectifyWrapper<DatabaseManager, DatabaseFactory> {
-
-		public DatabaseManager(Objectify ofy) {
-			super(ofy);
-		}
-
-		@Override
-		public DatabaseLoader load() {
-			return new DatabaseLoader(super.load());
-		}
-
 	}
 
 	@Singleton
@@ -67,21 +51,6 @@ public abstract class DatabaseService {
 		public <T> T construct(Class<T> type) {
 			return injector.getInstance(type);
 		}
-
-		@Override
-		public DatabaseManager begin() {
-			return new DatabaseManager(super.begin());
-		}
-
-	}
-
-	public static class DatabaseLoader extends LoaderWrapper<DatabaseLoader> {
-
-		public DatabaseLoader(Loader base) {
-			super(base);
-		}
-
-		// extend loader with logic, like finding certain kinds of stuff
 
 	}
 }
