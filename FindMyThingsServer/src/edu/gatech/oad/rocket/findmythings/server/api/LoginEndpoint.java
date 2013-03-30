4f265ae25153f78724e6aae1e36a4b7866ca5f9d
Zachary Waldowski <zwaldowski@gmail.com>
2013-03-30 17:54:51 -0400

    Stub for Login Endpoint. turns out we don't need javax.inject.

---
+++ FindMyThingsServer/src/edu/gatech/oad/rocket/findmythings/server/api/LoginEndpoint.java
@@ -0,0 +1,19 @@
+package edu.gatech.oad.rocket.findmythings.server.api;
+
+import com.google.inject.Singleton;
+
+import edu.gatech.oad.rocket.findmythings.server.BaseServlet;
+
+@Singleton
+public class LoginEndpoint extends BaseServlet {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 7162845201201231985L;
+
+	public LoginEndpoint() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
