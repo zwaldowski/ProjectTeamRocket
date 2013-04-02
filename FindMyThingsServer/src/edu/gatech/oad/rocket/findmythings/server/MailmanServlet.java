f038db00e39bcdd415a70066e47443b2670c04a6
Zachary Waldowski <zwaldowski@gmail.com>
2013-04-02 16:32:45 -0400

    Few things are as certain in life as me forgetting the @Singleton annotation.

--- FindMyThingsServer/src/edu/gatech/oad/rocket/findmythings/server/MailmanServlet.java
+++ FindMyThingsServer/src/edu/gatech/oad/rocket/findmythings/server/MailmanServlet.java
@@ -2,6 +2,9 @@ package edu.gatech.oad.rocket.findmythings.server;
 
 import javax.servlet.http.HttpServlet;
 
+import com.google.inject.Singleton;
+
+@Singleton
 public class MailmanServlet extends HttpServlet {
 
 }
