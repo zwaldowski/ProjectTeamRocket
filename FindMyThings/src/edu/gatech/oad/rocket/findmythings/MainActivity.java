8f34927c310bb0567b6b1d516c13d2d45ede74b1
Cristina Chu <cchu43@gatech.edu>
2013-04-03 21:40:08 -0300

    added search intent with layout

--- FindMyThings/src/edu/gatech/oad/rocket/findmythings/MainActivity.java
+++ FindMyThings/src/edu/gatech/oad/rocket/findmythings/MainActivity.java
@@ -79,16 +79,9 @@ public class MainActivity extends PreferenceActivity implements OnSharedPreferen
 	        Intent requestIntent = findPreference(getString(R.string.main_key_requests)).getIntent();
 	        requestIntent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
 	        requestIntent.putExtra(Type.ID, Type.REQUEST.ordinal());
-	        
-	        //trying new stuff
-	        //Intent searchIntent = findPreference(getString(R.string.main_key_searches)).getIntent();
-	        //searchIntent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-	        Intent searchIntent =  new Intent(getActivity(), Search_Main.class);
 	    
-	        //Intent(Context packageContext, Class<?> cls)
-	        
-	        //MainFragment.this,Search_Main.class
-	        //end
+	    	Intent searchIntent = findPreference(getString(R.string.main_key_searches)).getIntent();
+	        searchIntent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
 
 	        if(Login.currUser!=null) {
 	        	Preference myAccount = findPreference(getString(R.string.main_key_myaccount));
