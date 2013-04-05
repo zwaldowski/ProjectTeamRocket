08eb16c82b8db99704f9f38575ff894fb102aa59
jcole123 <jcole12321@gmail.com>
2013-04-04 20:12:36 -0400

    Clean up.

--- FindMyThings/src/edu/gatech/oad/rocket/findmythings/ItemListFragment.java
+++ FindMyThings/src/edu/gatech/oad/rocket/findmythings/ItemListFragment.java
@@ -145,9 +145,7 @@ public class ItemListFragment extends ListFragment {
 		// Notify the active callbacks interface (the activity, if the
 		// fragment is attached to one) that an item has been selected.
 		Type mItemClass = ((MainActivity)getActivity()).getItemType();
-		String s = currList.get(position).getName();
-		mCallbacks.onItemSelected(s);
-		//mCallbacks.onItemSelected(control.getItem(mItemClass, position).getName());
+		mCallbacks.onItemSelected(control.getItem(mItemClass, position).getName());
 	}
 
 	@Override
