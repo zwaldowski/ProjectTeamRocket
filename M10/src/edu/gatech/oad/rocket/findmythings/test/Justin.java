146060aa06fd101f2e636c464de5e4b59559c187
jcole123 <jcole12321@gmail.com>
2013-04-06 15:15:01 -0400

    Made my Test slightly less trivial.

--- M10/src/edu/gatech/oad/rocket/findmythings/test/Justin.java
+++ M10/src/edu/gatech/oad/rocket/findmythings/test/Justin.java
@@ -2,21 +2,22 @@ package edu.gatech.oad.rocket.findmythings.test;
 
 import edu.gatech.oad.rocket.findmythings.MyAccount;
 import edu.gatech.oad.rocket.findmythings.MyAccountEdit;
+import edu.gatech.oad.rocket.findmythings.NonActivity.Login;
 import edu.gatech.oad.rocket.findmythings.R;
+import edu.gatech.oad.rocket.findmythings.NonActivity.Member;
+import edu.gatech.oad.rocket.findmythings.NonActivity.User;
 import android.app.Activity;
 import android.app.Instrumentation.ActivityMonitor;
 import android.test.ActivityInstrumentationTestCase2;
 import android.view.KeyEvent;
-import android.view.Menu;
+import android.widget.EditText;
 
-public class Justin extends ActivityInstrumentationTestCase2<MyAccount> {
+public class Justin extends ActivityInstrumentationTestCase2<MyAccountEdit> {
 	
-	private MyAccount mActivity;
-	
-	private Menu mBut;
+	private MyAccountEdit mActivity;
 	
 	public Justin() {
-		 super("andtest.threads.asynctask", MyAccount.class);
+		 super("andtest.threads.asynctask", MyAccountEdit.class);
 	}
 
 	@Override
@@ -24,20 +25,42 @@ public class Justin extends ActivityInstrumentationTestCase2<MyAccount> {
         super.setUp();
         mActivity = getActivity();
     }
-	public void testMenu() {
+	public void testMenu() throws Throwable {
+		String email = "test@test.test";
+		String pass = "test";
+		//Test user
+		Member toTest = new User(email, pass);
+		toTest.setName("Doug");
+		
+		Login.currUser = toTest;
+				
 		//Menu button should go to MyAccountEdit Activity
-		ActivityMonitor monitor = getInstrumentation().addMonitor(MyAccountEdit.class.getName(), null, false);
-
+		ActivityMonitor monitor = getInstrumentation().addMonitor(MyAccount.class.getName(), null, false);
+		
+		
+		runTestOnUiThread(new Runnable() {
+		     public void run() {
+		    	//Get current name in textfield
+		    	 EditText name = (EditText)mActivity.findViewById(R.id.personnameedit);
+		 		//Set new name
+		 		name.setText("Funny");
+		     }
+		});
+	
 		//Select menu button
 		getInstrumentation().sendKeyDownUpSync(KeyEvent.KEYCODE_MENU);
-		getInstrumentation().invokeMenuActionSync(mActivity, R.id.toEdit, 0);
+		getInstrumentation().invokeMenuActionSync(mActivity, R.id.edit_ok, 0);
 		
-		Activity toEditActivity = getInstrumentation().waitForMonitorWithTimeout(monitor, 1000);
-		//If 1, then it went to the Activity
+		Activity toMyAccount = getInstrumentation().waitForMonitorWithTimeout(monitor, 1000);
+		//Check if MyAccount Activity was started
 		assertEquals(true, getInstrumentation().checkMonitorHit(monitor, 1));
-		toEditActivity.finish();
+		assertEquals("Funny", Login.currUser.getName());
+		toMyAccount.finish();
 	}
-	
+		
+		
+		
+			
 	
 	
 
