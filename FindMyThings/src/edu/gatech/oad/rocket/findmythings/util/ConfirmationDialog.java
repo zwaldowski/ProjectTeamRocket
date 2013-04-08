cd096a067a9007eb402f0798eb658854c403fff7
Cristina Chu <cchu43@gatech.edu>
2013-04-08 12:35:51 -0400

    javadoc for util done

--- FindMyThings/src/edu/gatech/oad/rocket/findmythings/util/ConfirmationDialog.java
+++ FindMyThings/src/edu/gatech/oad/rocket/findmythings/util/ConfirmationDialog.java
@@ -6,6 +6,8 @@ import android.preference.DialogPreference;
 import android.util.AttributeSet;
 
 /**
+ * CS 2340 - FindMyStuff Android App
+ * 
  * The {@link ConfirmationDialog} is a preference subclass that shows
  * a dialog with Yes and No buttons, for the purpose of confirming an action.
  *
@@ -15,16 +17,26 @@ import android.util.AttributeSet;
  * Dervied from:
  * - http://stackoverflow.com/questions/5365310/creating-a-dialogpreference-from-xml/8818446#8818446
  * - YesNoPreference.java in Android Private SDK
- *
+ * 
+ * @author TeamRocket
  */
 public class ConfirmationDialog extends DialogPreference {
     /** The Yes/No value of the box. */
     private boolean mValue;
 
+    /**
+     * constructor
+     * @param context
+     * @param attrs
+     */
     public ConfirmationDialog(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
+    /**
+     * deals with action to do once dialog is closed
+     * @param boolean positiveResult
+     */
     @Override
     protected void onDialogClosed(boolean positiveResult) {
         super.onDialogClosed(positiveResult);
@@ -34,17 +46,29 @@ public class ConfirmationDialog extends DialogPreference {
         }
     }
 
+    /**
+     * @param TypedArray a
+     * @param int index
+     * @return Object
+     */
     @Override
     protected Object onGetDefaultValue(TypedArray a, int index) {
         return a.getBoolean(index, false);
     }
 
+    /**
+     * @param boolean restorePersistedValue
+     * @param Object defaultValue
+     */
     @Override
     protected void onSetInitialValue(boolean restorePersistedValue, Object defaultValue) {
         setValue(restorePersistedValue ? getPersistedBoolean(mValue) :
                 (Boolean) defaultValue);
     }
 
+    /**
+     * @return boolean
+     */
     @Override
     public boolean shouldDisableDependents() {
         return !mValue || super.shouldDisableDependents();
--- FindMyThings/src/edu/gatech/oad/rocket/findmythings/util/MainTabListener.java
+++ FindMyThings/src/edu/gatech/oad/rocket/findmythings/util/MainTabListener.java
@@ -7,21 +7,41 @@ import android.app.ActionBar.Tab;
 import android.app.ActionBar.TabListener;
 import android.app.FragmentTransaction;
 
+/**
+ * CS 2340 - FindMyStuff Android App
+ * deals with actions of the tabbed menu
+ *
+ * @author TeamRocket
+ * */
 public class MainTabListener implements TabListener{
 	
 	private String name;
 	
 	private Controller control = Controller.shared();
 	
+	/**
+	 * constructor
+	 * @param name
+	 */
 	public MainTabListener(String name) {
 		this.name = name;
 	}
+	
+	/**
+	 * deals with action when a tab is reselected
+	 * @param Tab tab
+	 * @param FragmentTransaction ft
+	 */
 	@Override
 	public void onTabReselected(Tab tab, FragmentTransaction ft) {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub	
 	}
 
+	/**
+	 * deals with action to take when a tab is selected
+	 * @param Tab tab
+	 * @param FragmentTransaction ft
+	 */
 	@Override
 	public void onTabSelected(Tab tab, FragmentTransaction ft) {
 		if(name.equals("ALL")) {
@@ -46,6 +66,11 @@ public class MainTabListener implements TabListener{
 		
 	}
 
+	/**
+	 * deals with action to take when a tab is not selected
+	 * @param Tab tab
+	 * @param FragmentTransaction ft
+	 */
 	@Override
 	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
 		// TODO Auto-generated method stub
