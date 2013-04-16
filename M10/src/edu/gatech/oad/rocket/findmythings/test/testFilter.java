9869223adfe14c0b32d4b6ed0577224be5f9584d
Cristina Chu <cchu43@gatech.edu>
2013-04-16 12:30:38 -0300

    manually updated testFilter.java

--- M10/src/edu/gatech/oad/rocket/findmythings/test/testFilter.java
+++ M10/src/edu/gatech/oad/rocket/findmythings/test/testFilter.java
@@ -2,14 +2,13 @@ package edu.gatech.oad.rocket.findmythings.test;
 
 import java.util.ArrayList;
 
+import android.app.ActionBar;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.InstrumentationTestCase;
-import android.view.KeyEvent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
 import edu.gatech.oad.rocket.findmythings.FilterActivity;
-import edu.gatech.oad.rocket.findmythings.LoginActivity;
-import edu.gatech.oad.rocket.findmythings.control.Controller;
 import edu.gatech.oad.rocket.findmythings.model.Category;
 import edu.gatech.oad.rocket.findmythings.model.Item;
 import junit.framework.TestCase;
@@ -18,16 +17,17 @@ public class testFilter extends ActivityInstrumentationTestCase2<FilterActivity>
 
 	private FilterActivity activity;
 	private Spinner status, category, date;
-	private Button ok; 
-	
-	private ArrayList<Item> results, listItems;
-	private Controller cnt = Controller.shared();
+	private ActionBar bar;
+	private ActionBar.Tab ok; 
+
+	private ArrayList<Item> results;
 	
 	public static final int INITIAL_POS = 0;
 	public static final int TEST_POS_ONE = 1;
 	public static final int TEST_POS_TWO = 2;
 	public static final int TEST_POS_THREE = 3;
 	
+	@SuppressWarnings("deprecation")
 	public testFilter() {
 		super("andtest.threads.asynctask", FilterActivity.class);
 	}
@@ -39,9 +39,11 @@ public class testFilter extends ActivityInstrumentationTestCase2<FilterActivity>
 		status = (Spinner)activity.findViewById(edu.gatech.oad.rocket.findmythings.R.id.status_spinner);
 		category = (Spinner)activity.findViewById(edu.gatech.oad.rocket.findmythings.R.id.cat_spinner);
 		date = (Spinner)activity.findViewById(edu.gatech.oad.rocket.findmythings.R.id.date_spinner);
-		ok = (Button)activity.findViewById(edu.gatech.oad.rocket.findmythings.R.id.filter_ok);
+		//bar = (ActionBar)activity.findViewById();
+		//ok = (ActionBar.Tab)activity.findViewById(edu.gatech.oad.rocket.findmythings.R.id.).filter_ok);
 	}
 	
+	@SuppressWarnings("static-access")
 	public void testCase() { 
 	    // Spinners current position
 	    int statusCurrentPos = status.getSelectedItemPosition();
@@ -72,35 +74,29 @@ public class testFilter extends ActivityInstrumentationTestCase2<FilterActivity>
 	    
 	    /** Checking for filter results */
 	    /** Test Case 2 = Status=ALL, Category=KEEPSAKE, Date=ALL  */
-	    
-	    // results that should come up when filter is done
-	    /*
-	    listItems = cnt.getAllItems();
-	    for (Item i: listItems) {
-	    	if (i.getCat() != Category.KEEPSAKE) 
-	    		((ArrayList<Item>)listItems).remove(i);
-	    }*/
-	    
 	    activity.runOnUiThread(
 	    		new Runnable() {
 	    			public void run() {
 	    				status.setSelection(INITIAL_POS);
 	    				category.setSelection(TEST_POS_TWO);
 	    				date.setSelection(INITIAL_POS);
-	    				ok.performClick();
+	    				//ok.performClick();
 	    			}
 	    		});
 	    
 	    //getting the results after selection and submiting
 	    results = activity.toReturn;
 	    
-	    assertEquals(3, results.size());
-	    
-	    for (Item i: results) {
-	    	assertEquals(Category.KEEPSAKE, i.getCat());
+	    int counter = 0;
+	    for (int i =0; i<results.size(); i++) {
+	    	if (results.get(i).getCat() == Category.KEEPSAKE) {
+	    		counter++;
+	    	}
 	    }
-	   
 	    
+	    // if counter == results.size, it means all the things in results are KEEPSAKES
+	    assertEquals(counter, results.size());
+	    assertEquals(3,results.size()); //there should be 3 items
 	}
 }
 
