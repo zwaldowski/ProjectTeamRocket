2a607f45c314d74b5584823b24c6ed73a1eaa5c4
Tyler Stowell <tstowell3@gatech.edu>
2013-04-22 14:10:14 -0300

    Fixed NullPointerException and added assertFalse

--- M10/src/edu/gatech/oad/rocket/findmythings/test/Tyler2.java
+++ M10/src/edu/gatech/oad/rocket/findmythings/test/Tyler2.java
@@ -1,6 +1,5 @@
 package edu.gatech.oad.rocket.findmythings.test;
 
-import android.app.Activity;
 import android.app.Instrumentation.ActivityMonitor;
 import android.test.ActivityInstrumentationTestCase2;
 import android.view.KeyEvent;
@@ -28,34 +27,33 @@ public class Tyler2 extends ActivityInstrumentationTestCase2<MainActivity> {
 	
 	public void testMenu() throws Throwable {
 		
+		// Make sure there is no currently logged in user
 		LoginManager loginMan = LoginManager.getLoginManager();
-		loginMan.logout();
-		
+		loginMan.logout();	
 		assertFalse(LoginManager.getLoginManager().isLoggedIn());
-		ActivityMonitor monitor = getInstrumentation().addMonitor(MainActivity.class.getName(), null, false);
-		getInstrumentation().invokeMenuActionSync(mActivity, R.id.item_list_submit, 0);
-
-		getInstrumentation().sendKeyDownUpSync(KeyEvent.KEYCODE_DPAD_DOWN);
-	  getInstrumentation().sendKeyDownUpSync(KeyEvent.KEYCODE_DPAD_CENTER);
-		Activity toMain = getInstrumentation().waitForMonitorWithTimeout(monitor, 1000);
 		
-		AppMember temp = new AppMember();
+		// Attempt to submit an item anonymously and check for failure
+		ActivityMonitor monitor = getInstrumentation()
+				.addMonitor(MainActivity.class.getName(), null, false);
+		assertFalse(getInstrumentation().checkMonitorHit(monitor, 1));
+		getInstrumentation().invokeMenuActionSync
+				(mActivity, R.id.item_list_submit, 0);
+		getInstrumentation().sendKeyDownUpSync(KeyEvent.KEYCODE_DPAD_DOWN);
+	    getInstrumentation().sendKeyDownUpSync(KeyEvent.KEYCODE_DPAD_CENTER);
+		
+	    // Log in a user and check to make sure it succeeded
+	    AppMember temp = new AppMember();
 		temp.setName("Spongebob");
 		temp.setEmail("valid@not.com");
 		loginMan.setCurrentUser(temp);
 		loginMan.setCurrentEmailAndToken("valid@not.com", "Spongebob");
 		assertTrue(LoginManager.getLoginManager().isLoggedIn());
-		getInstrumentation().invokeMenuActionSync(mActivity, R.id.item_list_submit, 1000);
-		assertEquals(true, getInstrumentation().waitForMonitorWithTimeout(monitor, 10) instanceof SubmitActivity);
+		
+		// Attempt once more to submit an item and check for success
+		ActivityMonitor monitor2 = getInstrumentation()
+				.addMonitor(SubmitActivity.class.getName(), null, false);
+		getInstrumentation().invokeMenuActionSync(mActivity, R.id.item_list_submit, 0);
+		assertTrue(getInstrumentation().checkMonitorHit(monitor2, 1));
 		loginMan.logout();
-		toMain.finish();
-		
-		
-		mActivity.runOnUiThread(new Runnable() {
-		     public void run() {
-		    	 
-		    	 
-		     }
-		});		
 	}
 }
